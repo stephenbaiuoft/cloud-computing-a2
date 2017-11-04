dd49321f0120f9eef28396633ba93e1f9c6dee23
avispeng <hypeng1993@gmail.com>
2017-11-03 23:20:29 -0400

    add functions

--- app/manager_ui.py
+++ app/manager_ui.py
@@ -19,11 +19,11 @@ def main():
     # create connection to ec2 worker pool
     ec2 = boto3.resource('ec2')
     # list a list of instances named 'worker'
-    workers = ec2.instances.filter(Filters=[{'Name':'instance-state-name', 'Values':['running']},
-                                            {'Name':'tag:Name', 'Values':['worker']}])
+    workers = ec2.instances.filter(Filters=[{'Name':'tag:Name', 'Values':['worker']}])
+    # {'Name':'instance-state-name', 'Values':['running']}
     cpu = []
     for instance in workers:
-        cpu.append(cpu_load(instance.id))
+        cpu.append(cpu_load(instance.id)[0])
 
     return render_template("manager_ui.html", title="Manager UI", instances_cpu = zip(workers, cpu))
 
@@ -88,7 +88,7 @@ def shrink_by_one():
     # check if there is any workers
     if len(workers) >= 1:
         last = workers[-1].id
-    ec2.instances.filter(InstanceIds=last).terminate()
+        ec2.instances.filter(InstanceIds=last).terminate()
     return redirect(url_for('main'))
 
 
@@ -98,6 +98,25 @@ def delete_all():
     # delete everything on s3
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('cloud-computing-photo-storage')
+    response = bucket.delete_objects(
+        Delete={
+            'Objects': [
+                {
+                    'Key': '*',
+                },
+            ],
+        },
+    )
+    # delete everthing in database
+    cnx = get_db()
+    cursor = cnx.cursor()
+    query = '''
+    DELETE FROM images WHERE img_id >= 1;
+    DELETE FROM users WHERE user_id >= 1;
+    '''
+    cursor.execute(query)
+    cnx.commit()
+
     return redirect(url_for('main'))
 
 
@@ -106,7 +125,7 @@ def cpu_load(id):
     client = boto3.client('cloudwatch')
 
     now = datetime.utcnow()
-    past = now - timedelta(minutes=30)
+    past = now - timedelta(minutes=60)
     future = now + timedelta(minutes=0)
 
     results = client.get_metric_statistics(
@@ -115,14 +134,30 @@ def cpu_load(id):
         Dimensions=[{'Name':'InstanceId', 'Value':id}],
         StartTime=past,
         EndTime=future,
-        Period=300,
+        Period=60,
         Statistics=['Average']
     )
-
+    cpu_stats = []
     datapoints = results['Datapoints']
+    # cpu utilization figure
+    for point in datapoints:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute / 60
+        cpu_stats.append([time, point['Average']])
+    cpu_stats = sorted(cpu_stats, key=itemgetter(0))
     # sort in timestamp order and fetch the latest one
-    last_datapoint = sorted(datapoints, key=itemgetter('Timestamp'))[-1]
-    utilization = last_datapoint['Average']
-    load = round((utilization/100.0), 2)
+    # in initializing phase, display 0 as cpu utilization
+    if len(cpu_stats) > 0:
+        last_datapoint = cpu_stats[-1][1]
+        load = round((last_datapoint / 100.0), 2)
+    else:
+        load = 0.
 
-    return load
+    return [load, cpu_stats]
+
+@webapp.route('worker_list/<id>', method=['GET'])
+def cpu_plot(id):
+    cpu_stats = cpu_load(id)[1]
+    return render_template("cpu_utilization.html", title="CPU Utilization Plot",
+                           cpu_stats=cpu_stats)
