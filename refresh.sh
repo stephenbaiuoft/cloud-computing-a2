d58043f3633bd5ebcef869226b1ac1e9e6a42f2a
avispeng <hypeng1993@gmail.com>
2017-11-05 00:54:30 -0400

    fix bugs about auto refresh

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-source ./Desktop/ece1779/manager/a2_manager_ui/venv/bin/activate
-python3 ./Desktop/ece1779/manager/a2_manager_ui/refresh.py
+cd ./Desktop/ece1779/manager/a2_manager_ui
+source venv/bin/activate
+python3 refresh.py
 
 
