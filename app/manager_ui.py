9216ed241f15ec5e5c596a11680bd8afa49b544b
avispeng <hypeng1993@gmail.com>
2017-11-05 00:22:57 -0400

    fix bugs in auto scaling

--- app/manager_ui.py
+++ app/manager_ui.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request, g
 from app import webapp
 
 import boto3
-import math
+# import math
 from app import config
 from datetime import datetime, timedelta
 from operator import itemgetter
@@ -209,68 +209,4 @@ def cpu_plot(id):
                            cpu_stats=cpu_stats)
 
 
-@webapp.route('/auto_refresh')
-def auto_refresh():
 
-    cpu_threshold_high = CPU_THRE_H
-    cpu_threshold_low = CPU_THRE_L
-    ratio_grow = RATIO_GROW
-    ratio_shrink = RATIO_SHRINK
-
-    # create connection to ec2 worker pool
-    ec2 = boto3.resource('ec2')
-    # list a list of instances named 'worker'
-    workers = ec2.instances.filter(Filters=[{'Name': 'tag:Name', 'Values': ['worker']},
-                                            {'Name': 'instance-state-name',
-                                             'Values': ['running']}])
-    cpu_sum = 0
-    count = 0
-    for instance in workers:
-        cpu_sum += cpu_load(instance.id)[0]
-        count += 1
-    average_current = cpu_sum / count
-    if average_current > cpu_threshold_high:
-        global MAIN_MSG
-        num_to_grow = math.floor(ratio_grow * count - count)
-        if num_to_grow > 0:
-            new_instances = ec2.create_instances(ImageId=config.ami_id,
-                                                MinCount=num_to_grow,
-                                                MaxCount=num_to_grow,
-                                                InstanceType=config.instance_type,
-                                                KeyName=config.key_name,
-                                                Monitoring=config.monitoring,
-                                                SecurityGroupIds=config.security_group,
-                                                SubnetId=config.subnet,
-                                                UserData=config.userdata,
-                                                IamInstanceProfile=config.iam_instance_profile,
-                                                TagSpecifications=config.tag_specification
-                                                )
-            # attach them to load balancer
-            new_ids = []
-            for new_worker in new_instances:
-                new_ids.append({'InstanceId':new_worker.id})
-            elb = boto3.client('elb')
-            response = elb.register_instances_with_load_balancer(
-                LoadBalancerName=config.elbname,
-                Instances=new_ids
-            )
-            MAIN_MSG = 'Automatically expanded the worker pool by ratio '+str(ratio_grow)
-    elif average_current < cpu_threshold_low:
-        global MAIN_MSG
-        num_to_shrink = math.floor(count - count * 1. / ratio_shrink)
-        if num_to_shrink > 0 and count > 1:
-            elb = boto3.client('elb')
-            delete_ids = []
-            for worker in workers:
-                worker.terminate()
-                delete_ids.append({'InstanceId': worker.id})
-                num_to_shrink -= 1
-                if num_to_shrink == 0:
-                    break
-            # detach from load balancer
-            response = elb.deregister_instances_from_load_balancer(
-                LoadBalancerName=config.elbname,
-                Instances=delete_ids
-            )
-            MAIN_MSG = 'Automatically shrank the worker pool by ratio ' + str(ratio_shrink)
-    return redirect(url_for('main'))
